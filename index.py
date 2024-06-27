5ec53a6142319eecc26e7c55e7055879de568bc0
SHAHADAT HOSSEN SHUVO <2027shuvo@gmail.com>
2024-06-27 16:26:23 +0600

    tip-percentage fixed

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 
 print("Welcome to the Tip Calculator!")
 
-total_bill = float(input("What was the total bill?"))
+bill = float(input("What was the total bill?"))
 
 tip = int(input("How much tip would you like to give? 10,15,20?"))
 
@@ -10,14 +10,16 @@ people = int(input("How many people to split the bill?"))
 
 tip_as_percentage = tip/100
 
-bill_with_tip = total_bill + tip_as_percentage
+tip_amount = bill * tip_as_percentage
+
+total_bill = bill + tip_amount
 
 each_person_pays = total_bill/ people
 
-# final_amount = round(each_person_pays, 2)
+final_amount = round(each_person_pays, 2)
 
-final_amount = "{:.2f}".format(each_person_pays)
+# final_amount = "{:.2f}".format(each_person_pays)
 
 print("Each person should pay : $", final_amount)
 
-# print("Each person should pay: $")
+# print(f"Each person should pay: ${final_amount}")
